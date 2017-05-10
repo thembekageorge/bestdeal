fd860222c177d912b0d19f6c4b4555e01fa94b39
thembekageorge <thembiegeorge@gmail.com>
2017-05-10 15:06:07 +0200

    first commit

---
+++ avoPrice.js
@@ -0,0 +1,22 @@
+var givenString = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
+
+function bestdeal(string){
+
+ var splitString = string.split(',');
+
+ var newArr = [];
+
+for (var i = 0; i < splitString.length; i++) {
+
+  var splitDeal = splitString[i].split('for');
+
+ var numberofAvoes = splitDeal[0];
+
+ var dealPrice = splitDeal[1].replace("R","");
+
+  var priceOfAvo = Number(dealPrice/numberofAvoes).toFixed(2);
+
+   
+}
+}
+console.log(BestOfDeal(givenString));
